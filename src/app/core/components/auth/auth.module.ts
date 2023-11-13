dde62de63ed7730cf7ff24b124be3b2957df2236
ipv <ipv@vimansca.local>
2023-11-13 15:02:33 -0300

    Realiza comunicacao entre o backend e o frontend para validar as entradas de email e senha.

--- src/app/core/components/auth/auth.module.ts
+++ src/app/core/components/auth/auth.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignComponent } from './pages/sign/sign.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SignComponent],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-  ],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
