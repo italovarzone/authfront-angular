69f2598852fad67c3d6611de29470359009bb8bc
ipv <ipv@vimansca.local>
2023-11-13 15:26:03 -0300

    Realiza as configuracoes de acesso de sucesso ao backend.

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, catchError, map, throwError } from 'rxjs';
 
 @Injectable({
@@ -7,17 +8,32 @@ import { Observable, catchError, map, throwError } from 'rxjs';
 })
 export class AuthService {
   private url: string = 'http://localhost:3000';
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   public sign(payload: { email: string; password: string }): Observable<any> {
-    return this.http.post(`${this.url}/sign`, payload).pipe(
-      map(data => {
-        return console.log(data);
+    return this.http.post<{ token: string }>(`${this.url}/sign`, payload).pipe(
+      map(res => {
+        localStorage.removeItem('access_token');
+        localStorage.setItem('acess_token', res.token);
+        return this.router.navigate(['admin']);
       }),
-      catchError(err => {
-        console.log(err);
-        return throwError(() => err.error.message);
+      catchError(e => {
+        console.log(e);
+        if (e.error.message) return throwError(() => e.error.message);
+
+        return throwError(
+          () =>
+            'No momento não estamos conseguindo validar esses dados, tente novamente mais tarde!'
+        );
       })
     );
+  }
+
+  public logout() {
+    localStorage.removeItem('access_token');
+    return this.router.navigate(['']);
   }
 }
