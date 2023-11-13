2c10b71661e84aebd20de0fa2088ec150c80f537
ipv <ipv@vimansca.local>
2023-11-13 15:48:40 -0300

    Aidiciona o guard nas paginas de autenticacao admin.

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,13 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, catchError, map, throwError } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
+
+// Services
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private url: string = 'http://localhost:3000';
+
   constructor(
     private http: HttpClient,
     private router: Router
@@ -17,16 +21,15 @@ export class AuthService {
     return this.http.post<{ token: string }>(`${this.url}/sign`, payload).pipe(
       map(res => {
         localStorage.removeItem('access_token');
-        localStorage.setItem('acess_token', res.token);
+        localStorage.setItem('access_token', res.token);
         return this.router.navigate(['admin']);
       }),
       catchError(e => {
-        console.log(e);
         if (e.error.message) return throwError(() => e.error.message);
 
         return throwError(
           () =>
-            'No momento não estamos conseguindo validar esses dados, tente novamente mais tarde!'
+            'No momento não estamos conseguindo validar este dados, tente novamente mais tarde!'
         );
       })
     );
@@ -35,5 +38,14 @@ export class AuthService {
   public logout() {
     localStorage.removeItem('access_token');
     return this.router.navigate(['']);
+  }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('access_token');
+
+    if (!token) return false;
+
+    const jwtHelper = new JwtHelperService();
+    return !jwtHelper.isTokenExpired(token);
   }
 }
