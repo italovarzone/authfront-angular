1d317d5043bad97fb1bac6bad87f6bb5f2a73b8c
ipv <ipv@vimansca.local>
2023-11-13 14:44:04 -0300

    Adiciona metodos para enviar os dados do reativeForms para trabalhar no backend do projeto

--- src/app/core/components/auth/pages/sign/sign.component.ts
+++ src/app/core/components/auth/pages/sign/sign.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign',
   templateUrl: './sign.component.html',
-  styleUrls: ['./sign.component.scss']
+  styleUrls: ['./sign.component.scss'],
 })
-export class SignComponent {
+export class SignComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
 
+  public formAuth: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+  ngOnInit(): void {}
+
+  public submitForm() {
+    if (this.formAuth.valid) {
+      console.log(this.formAuth);
+    }
+  }
 }
